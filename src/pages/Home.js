ecf318bd69dd77bde1fc144dd9f64216916c2b54
saqlain-codecraf <mushtaqasm786@gmail.com>
2023-02-15 18:51:04 +0500

    dynamic codes

---
+++ src/pages/Home.js
@@ -0,0 +1,80 @@
+import "./style.css";
+function Home(){
+    return(
+    <>
+    <section className="home-section">
+        <div className="home-container">
+            <div>
+                <h3>AAdmirals Travel & Transportation Houston Limo Service </h3>
+            </div>
+            <div>
+                <p>AAdmirals provides superior Houston George Bush Airport Transport black car service, including a George Bush Airport shuttle service, guaranteed to be on time. Our professional chauffeurs are vetted, background-checked, and experienced, and our vehicles are pristine late models and fully insured. Trust AAdmirals to find the most efficient routes as you ride in comfort. Free 60 minutes wait time for international flights, 30 minutes for domestic flights arrivals at airport pickups and 15 mins for all others.</p>
+
+                <p> AAdmirals provides superior Houston George Bush Airport Transport black car service, including a George Bush Airport shuttle service, guaranteed to be on time. Our professional chauffeurs are vetted, background-checked, and experienced, and our vehicles are pristine late models and fully insured. Trust AAdmirals to find the most efficient routes as you ride in comfort. Free 60 minutes wait time for international flights, 30 minutes for domestic flights arrivals at airport pickups and 15 mins for all others.</p>
+            </div>
+        </div>
+        <div className="home-logo-section">
+            <div>
+                <img src="Group 4.svg"/>
+            </div>
+            <div>
+                <img src="Group 5.svg"/>
+            </div>
+            <div>
+                <img src="Group 6.svg"/>
+            </div>
+        </div>
+    </section>
+    <section >
+        <div className="container">
+            <div className="content-container">
+                <div>
+                    <h5>STRESS-FREE IAH AIRPORT TRANSPORTATION</h5>
+                </div>
+                <div>
+                    <h3>Get first-class service to and from the airport with AAdmirals</h3>
+                </div>  
+                <div>
+                    <p>AAdmirals provides superior Houston George Bush Airport Transport black car service, including a George Bush Airport shuttle service, guaranteed to be on time. Our professional chauffeurs are vetted, background-checked, and experienced, and our vehicles are pristine late models and fully insured. Trust AAdmirals to find the most efficient routes as you ride in comfort. Free 60 minutes wait time for international flights, 30 minutes for domestic flights arrivals at airport pickups and 15 mins for all others.</p>
+                    <p> AAdmirals provides superior Houston George Bush Airport Transport black car service, including a George Bush Airport shuttle service, guaranteed to be on time. Our professional chauffeurs are vetted, background-checked, and experienced, and our vehicles are pristine late models and fully insured. Trust AAdmirals to find the most efficient routes as you ride in comfort. Free 60 minutes wait time for international flights, 30 minutes for domestic flights arrivals at airport pickups and 15 mins for all others.</p>
+                </div>
+                <br/>
+                <div className="button-container">
+                    <button>
+                        Read More
+                    </button>
+                </div>
+            </div>
+            <div className="image-container">
+                    <img src="group-2371@2x.png" style={{height:'fit-content',width:'45vw'}}/>    
+            </div>
+        </div>
+    </section>
+    <section className="section2" >
+        <div className="container2">
+            <div className="image-container2">
+                    <img src="group-2372@2x.png" style={{height:'fit-content',width:'45vw'}}/>    
+            </div>
+            <div className="content-container2">
+                <div>
+                    <h3>Houston- limo- service- IAH- Airport</h3>
+                </div>  
+                <div>
+                    <p>We provide our quality limo services, to and from George Bush airport transport including Car service IAH Airport, George Bush Airport shuttle service, IAH Airport, one of the busiest airports in the US and the largest international hub in Texas with five terminals, IAH terminal A, IAH terminal B, IAH terminal C for domestic flights, IAH terminal D and IAH terminal E for international flights. IAH airport is also the second largest hub for United airlines in the US, with many regionals operating its domestic fleets and with American Airlines, Delta, Spirit and all the international flights it gets each day, IAH airport also is a destination for many international airlines such as Lufthansa, British Airways, KLM, Turkish Airlines, Qatar Airways, Emirates, ANA and many other international airlines deplaning in IAH airport daily. Whether you are going locally to IAH or through one of the international airlines, AAdmirals Travel& Transportation specializes in serving all your airport transportation needs. Our professional chauffeurs are continuously tracking weather, traffic and flight information that may affect your trip, and they always ensure that you arrive at your destination safe and on time and relaxed.
+We provide our quality limo services, to and from George Bush airport transport including Car service IAH Airport, George Bush Airport shuttle service, IAH Airport, one of the busiest airports in the US and the largest international hub in Texas with five terminals, IAH terminal A, IAH terminal B, IAH terminal C for domestic flights, IAH terminal D and IAH terminal E for international flights. IAH airport is also the second largest hub for United airlines in the US, </p>
+                    
+                </div>
+                <br/>
+                <div className="button-container2">
+                    <button>
+                        Read More
+                    </button>
+                </div>
+            </div>
+           
+        </div>
+    </section>
+    </>
+    );
+}
+export default Home;
